c43a207eda39774764c386db219557a5d484956b
Pietro <beelbonacossa@gmail.com>
2025-08-05 22:18:23 -0300

    pequeño fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,6 @@ export default function RootLayout({
       <head>
         {/* Favicons mejorados */}
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
         
